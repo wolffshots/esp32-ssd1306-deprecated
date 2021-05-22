7153ebcd4b216e4078fceaf489b08a60e80c10c8
jadon <16850875+wolffshots@users.noreply.github.com>
2021-05-22 21:39:31 +0200

    initial functional commit

---
+++ include/ssd1306.h
@@ -0,0 +1,6 @@
+#ifndef SSD1306_H
+#define SSD1306_H
+
+void ssd1306_init(void);
+
+#endif // SSD1306_H
---
+++ ssd1306.c
@@ -0,0 +1,7 @@
+#include "ssd1306.h"
+/**
+ * init placeholder
+ */
+void ssd1306_init(void)
+{
+}
